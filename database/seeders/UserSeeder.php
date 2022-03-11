e691056f4f2a30963a0a25ab72a5baf22c5501bd
chathurakodikara <chathura321@gmail.com>
2022-03-11 21:28:43 +0530

    update and add UserSeeder to DatabaseSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class UserSeeder extends Seeder
 {
@@ -15,7 +16,9 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
         User::truncate();
+        Schema::enableForeignKeyConstraints();
         User::create([
             'name' => 'Chathura',
             'email' => 'admin@gmail.com',
