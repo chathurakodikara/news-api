f99ef5915462806350536a7326091db80f46241f
chathurakodikara <chathura321@gmail.com>
2022-03-11 07:35:24 +0530

    updated page header

--- resources/views/headline/index.blade.php
+++ resources/views/headline/index.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            News Feeds
+            Top Headlines
         </h2>
+        <p>Exports selected data to the system</p>
     </x-slot>
 
     <div class="py-12">
