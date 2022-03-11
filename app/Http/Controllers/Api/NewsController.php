011151466b9960f97b62fcf0ddc22a6a163e677b
chathurakodikara <chathura321@gmail.com>
2022-03-11 07:31:44 +0530

    API end porint for news index and show

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -4,23 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\News;
 use Illuminate\Http\Request;
+use App\Services\NewsService;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\NewsCollection;
+use App\Http\Resources\NewsResource;
+use App\Http\Resources\NewsIndexCollection;
+use App\Http\Requests\NewsIndexRequest;
 
 class NewsController extends Controller
 {
-    public function index(Request $request)
+    public function index(NewsIndexRequest $request)
     {
-        $news = News::when($request->search, function ($query, $search)
-        {
-            $query->where('title', 'like', "%{$search}%")
-                ->orWhere('source', 'like', "%{$search}%")
-                ->orWhere('author', 'like', "%{$search}%");
-        })
-        ->whereDate('published_at', '<=', $request->from_date)
-        ->whereDate('published_at', '>=', $request->to_date)
-        ->paginate(10);
+        $news = NewsService::search($request->validated());
+        return new NewsIndexCollection($news);
+    }
 
-        return new NewsCollection($news);
+    public function show(News $news)
+    {
+        return new NewsResource($news);
     }
 }
